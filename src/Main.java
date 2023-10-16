e48e5415c2db52bd67294bb3e8e667d47f4ce3b6
jibek12 <romanbekovazibek@gmail.com>
2023-10-16 09:20:43 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount(0.0);
+        account.deposit(15000);
+        while (true) {
+            try {
+                account.withDraw(6000);
+                System.out.println("Снято 6000 сом, текущий баланс: " + account.getAmount() + " сом.");
+            } catch (LimitException e) {
+                try {
+                    account.withDraw(3000);
+                } catch (LimitException ex) {
+                    throw new RuntimeException(ex);
+                }
+                System.out.println("Исключение: " + e.getMessage());
+                System.out.println("Максимальная доступная сумма для снятия: " + e.getRemainingAmount() + " сом.");
+                System.out.println("Снято " + e.getRemainingAmount() + " сом. Остаток на счете: " + account.getAmount());
+                break;
+            }
+        }
+    }
+}
